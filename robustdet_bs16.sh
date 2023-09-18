ef904564d13baf08066b21cee5bda6fc72ed4023
LiangSiyuan21 <250846578@qq.com>
2023-09-18 09:43:55 +0800

    batchsize 16 and 32

--- robustdet_bs16.sh
+++ robustdet_bs16.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+#SBATCH --job-name=test1
+#SBATCH --partition=Model
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+source s0.3.5
+conda activate gan
+
 python -u train_adv.py --launcher=slurm --cfg cfgs/RobustDet_voc_bs16.yaml --adv_type mtd --data_use clean --multi_gpu True --basenet weights/ssd300_mAP_77.43_v2.pth
